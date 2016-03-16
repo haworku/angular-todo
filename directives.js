d5212ef06d27be6a89c7e2d801da39e51ca12252
Hana Worku <haworku@gmail.com>
2016-03-16 10:55:23 +0300

    Add comments

--- directives.js
+++ directives.js
@@ -4,7 +4,7 @@
 
   angular.module('app', [
   ]);
-
+ // sample code from phonecat project, don't understand whether I need filters or directives r what for refactoring
   .filter('strikethrough', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
