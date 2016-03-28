04640a003ac0417b8dee95336491c139588c019d
Hana Worku <haworku@gmail.com>
2016-03-28 17:04:31 +0300

    Clean up- removing counterController and app

--- controllers.js
+++ controllers.js
@@ -3,26 +3,10 @@
 
   angular.module('app', ['TodoService'])
 
-
-    .controller('counterController', counterController)
     .controller('todoController', todoController)
 
-
-    counterController.$inject = ['$scope']
     todoController.$inject = ['$log', '$filter', '$timeout', 'Todo']
 
-    function counterController($scope){
-      $scope.counter = 0;
-      console.log($scope)
-
-      $scope.add = function() {
-       $scope.counter >= 10 ? $scope.counter : $scope.counter ++;
-      }
-
-      $scope.sub = function(){
-        $scope.counter === 0 ? $scope.counter : $scope.counter --;
-      }
-    };
 
     function todoController($log, $filter, $timeout, Todo){
        var vm = this;
@@ -49,7 +33,7 @@
        vm.todoList  = Todo.complete(index);
         };
 
-      this.numberCompleted = function(){
+    vm.numberCompleted = function(){
          // Display updated completed/total fraction
          return vm.todoList.reduce(function(p, n){
           return n.completed === true ? p + 1 : p;
@@ -59,7 +43,6 @@
 
       this.toggleShowCompleted= function(){
         // Toggle visbility of completed tasks
-        this.showCompleted = !this.showCompleted
         this.toggleText = this.showCompleted ? "Hide Completed" : "Show Completed";
         };
 
