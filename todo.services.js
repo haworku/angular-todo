b9c31cbefed3cccd84e1f8994e764c75fd20f605
Hana Worku <haworku@gmail.com>
2016-03-29 11:14:19 +0300

    cleaning up file

--- todo.services.js
+++ todo.services.js
@@ -5,14 +5,9 @@
   angular.module('TodoService', []);
 
   angular
-    .module('TodoService')
+  .module('TodoService')
     .factory('Todo', Todo); // Telling Angular we're going to be have a service called `Todo`
 
-  /**
-   * JS TIP
-   * what we did on line 9 is known as currying.
-   * one of neat JavaScript *quirks* that'll allow us to make our code look smooth
-   */
   Todo.$inject = ['$rootScope']; // $rootScope as dependency.
 
   /**
@@ -26,16 +21,12 @@
    *
    * @param {$rootSCope} $rootScope - used for `$emit` and `$on`
    */
-  function Todo($rootScope) {
-    /**
-     * JS TIP
-     * what we're doing with `TodoList` and `EVENT_NAME` is known as closure.
-     * the variables are only acceded inside the `Todo` function
-     */
-    var TodoList = [];
-    var EVENT_NAME = 'HANA'; // event name that'll be *announced* on change
+   function Todo($rootScope) {
 
-    return {
+     var TodoList = [];
+     var EVENT_NAME = 'HANA'; // event name that'll be *announced* on change
+
+     return {
       /**
        * add task to our TodoList
        *
@@ -43,8 +34,8 @@
        */
 
        get() {
-          return Object.assign( [],TodoList );
-       },
+        return Object.assign( [],TodoList );
+      },
 
       add(task) {
         TodoList.push({
@@ -61,18 +52,18 @@
        * @param  {Number} index
        * @return {Object} updatedTask
        */
-      update(index, updatedTask) {
+       update(index, updatedTask) {
         // this is the equivalent of ECMAScript 6's Object.assign
         angular.extend(TodoList[index], updatedTask);
 
         return Object.assign( [],TodoList );
       },
          // Toggle 'complete' property in data structure
-      complete(index){
-         var completeBool = !TodoList[index].completed;
-         TodoList[index].completed = completeBool
-          return Object.assign( [],TodoList );
-      },
+         complete(index){
+           var completeBool = !TodoList[index].completed;
+           TodoList[index].completed = completeBool
+           return Object.assign( [],TodoList );
+         },
 
       /**
        * removes a chore at specified index
@@ -80,7 +71,7 @@
        * @param  {Number} index
        * @return {Array} updated current list
        */
-      remove(index) {
+       remove(index) {
         TodoList.splice(index, 1);
 
         return Object.assign( [],TodoList );
@@ -94,7 +85,7 @@
        * @param {Function} callback function that'll subscribe to change
        * @return {Function} unsubscribe function
        */
-      subscribe(scope, callback) {
+       subscribe(scope, callback) {
         var handler = $rootScope.$on(EVENT_NAME, callback);
         scope.$on('$destroy', handler);
 
