35e6941c7753a0d7ba8f2f8651c8623de0be9dc8
Hana Worku <haworku@gmail.com>
2016-03-29 12:29:49 +0300

    Fix indentation

--- controllers.js
+++ controllers.js
@@ -1,44 +1,44 @@
 (function (angular){
-  'use strict';
 
-  angular.module('app', ['TodoService'])
+    'use strict';
+    angular.module('app', ['TodoService'])
 
-    .controller('todoController', todoController)
+    .controller('todoController', todoController);
 
-    todoController.$inject = ['$log', '$filter', '$timeout', 'Todo']
+    todoController.$inject = ['$log', '$filter', '$timeout', 'Todo'];
 
 
     function todoController($log, $filter, $timeout, Todo){
-       var vm = this;
-       vm.todoList = Todo.get();
+        var vm = this;
+        vm.todoList = Todo.get();
 
 
-      vm.add = function(chore){
-        vm.todoList = Todo.add(chore);
-      };
-
-    vm.update = function(index, updatedTask){
-     vm.todoList  = Todo.update(index, updatedTask);
-    };
-
-    vm.remove = function(index){
-     vm.todoList  = Todo.remove(index);
-    };
-
-    vm.toggleComplete= function(index){
-       vm.todoList  = Todo.complete(index);
+        vm.add = function(chore){
+            vm.todoList = Todo.add(chore);
         };
 
-    vm.numberCompleted = function(){
-         return vm.todoList.reduce(function(p, n){
-          return n.completed === true ? p + 1 : p;
-        }, 0);
+        vm.update = function(index, updatedTask){
+            vm.todoList  = Todo.update(index, updatedTask);
+        };
 
-      };
+        vm.remove = function(index){
+            vm.todoList  = Todo.remove(index);
+        };
 
-      vm.toggleShowCompleted= function(){
+        vm.toggleComplete= function(index){
+            vm.todoList  = Todo.complete(index);
+        };
+
+        vm.numberCompleted = function(){
+            return vm.todoList.reduce(function(p, n){
+            return n.completed === true ? p + 1 : p;
+          }, 0);
+
+        };
+
+        vm.toggleShowCompleted= function(){
         // Toggle visbility of completed tasks
-        this.toggleText = this.showCompleted ? "Hide Completed" : "Show Completed";
+            this.toggleText = this.showCompleted ? 'Hide Completed' : 'Show Completed';
         };
 
     }
