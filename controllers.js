db3dd7a8e7ed6179520e63371031f05671a37f8d
Hana Worku <haworku@gmail.com>
2016-03-29 12:32:45 +0300

    fix identation

--- controllers.js
+++ controllers.js
@@ -1,45 +1,40 @@
 (function (angular){
-  'use strict';
 
-  angular.module('app', ['TodoService'])
+    'use strict';
+    angular.module('app', ['TodoService'])
 
-    .controller('todoController', todoController)
+    .controller('todoController', todoController);
 
-    todoController.$inject = ['$log', '$filter', '$timeout', 'Todo']
+    todoController.$inject = ['$log', '$filter', '$timeout', 'Todo'];
 
 
     function todoController($log, $filter, $timeout, Todo){
-       var vm = this;
-       vm.todoList = Todo.get();
+        var vm = this;
+        vm.todoList = Todo.get();
 
 
-      vm.add = function(chore){
-        vm.todoList = Todo.add(chore);
-      };
-
-    vm.update = function(index, updatedTask){
-     vm.todoList  = Todo.update(index, updatedTask);
-    };
-
-    vm.remove = function(index){
-     vm.todoList  = Todo.remove(index);
-    };
-
-    vm.toggleComplete= function(index){
-       vm.todoList  = Todo.complete(index);
+        vm.add = function(chore){
+            vm.todoList = Todo.add(chore);
         };
 
-    vm.numberCompleted = function(){
-         return vm.todoList.reduce(function(p, n){
-          return n.completed === true ? p + 1 : p;
-        }, 0);
-
-      };
-
-      vm.toggleShowCompleted= function(){
-        // Toggle visbility of completed tasks
-        this.toggleText = this.showCompleted ? "Hide Completed" : "Show Completed";
+        vm.update = function(index, updatedTask){
+            vm.todoList  = Todo.update(index, updatedTask);
         };
+
+        vm.remove = function(index){
+            vm.todoList  = Todo.remove(index);
+        };
+
+        vm.toggleComplete= function(index){
+            vm.todoList  = Todo.complete(index);
+        };
+
+        vm.numberCompleted = function(){
+            return vm.todoList.reduce(function(p, n){
+                return n.completed === true ? p + 1 : p;
+            }, 0);
+        };
+
 
     }
 
