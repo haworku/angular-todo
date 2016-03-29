7e393fb05a9f9f1b954fb90133a9de3b651f5eb0
Moe Szyslak <moe.duffdude@gmail.com>
2016-03-29 16:31:07 +0300

    no side effects inside the controller + EOF
    - mutations moved to the service

--- controllers.js
+++ controllers.js
@@ -1,41 +1,42 @@
 (function (angular){
-
     'use strict';
     angular.module('app', ['TodoService'])
 
     .controller('todoController', todoController);
 
-    todoController.$inject = ['$log', '$filter', '$timeout', 'Todo'];
+    todoController.$inject = ['$log', '$scope', '$filter', '$timeout', 'Todo'];
 
 
-    function todoController($log, $filter, $timeout, Todo){
-        var vm = this;
-        vm.todoList = Todo.get();
+    function todoController($log, $scope, $filter, $timeout, Todo){
+      var vm = this;
+      vm.chore = 'typing';
+      vm.editChore = '';
+      vm.todoList = Todo.get();
+      vm.numberCompleted = Todo.completed();
 
+      Todo.subscribe($scope, function() {
+        vm.numberCompleted = Todo.completed();
+      });
 
-        vm.add = function(chore){
-            vm.todoList = Todo.add(chore);
-        };
+      vm.add = function (chore) {
+        vm.todoList = Todo.add(vm.chore);
+        vm.chore = '';
+      };
 
-        vm.update = function(index, updatedTask){
-            vm.todoList  = Todo.update(index, updatedTask);
-        };
+      vm.update = function (index, updatedTask) {
+        vm.todoList = Todo.update(index, updatedTask);
+      };
 
-        vm.remove = function(index){
-            vm.todoList  = Todo.remove(index);
-        };
+      vm.remove = function (index) {
+        vm.todoList = Todo.remove(index);
+      };
 
-        vm.toggleComplete= function(index){
-            vm.todoList  = Todo.complete(index);
-        };
+      vm.toogleEdit = function(index) {
+        vm.todoList = Todo.update(index, {edit: !vm.todoList[index].edit});
+      };
 
-        vm.numberCompleted = function(){
-            return vm.todoList.reduce(function(p, n){
-                return n.completed === true ? p + 1 : p;
-            }, 0);
-        };
-
-
+      vm.toggleComplete = function (index) {
+        vm.todoList = Todo.update(index, {completed: !vm.todoList[index].completed});
+      };
     }
-
 })(window.angular);
